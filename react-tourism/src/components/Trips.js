7b1171da86ba6684c26a3e5e1ffef85a9f8b4c21
Fredrik <fredrikf2000@gmail.com>
2021-08-02 20:21:29 +0200

    started working on trips

---
+++ react-tourism/src/components/Trips.js
@@ -0,0 +1,54 @@
+import React from "react"
+import styled from "styled-components"
+import { useStaticQuery, graphql } from "gatsby"
+
+const Trips = () => {
+  const data = useStaticQuery(graphql`
+    query TripsQuery {
+      allTripsJson {
+        edges {
+          node {
+            alt
+            button
+            name
+            img {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  function getTrips(data) {
+    const tripsArray = []
+  }
+
+  return (
+    <ProductsContainer>
+      <ProductsHeading>Heading</ProductsHeading>
+      <ProductWrapper>Wrapper</ProductWrapper>
+    </ProductsContainer>
+  )
+}
+
+export default Trips
+
+const ProductsContainer = styled.div`
+  min-height: 100vh;
+  padding: 5rem calc((100vw - 1300px) / 2);
+  background: red;
+  color: #fff;
+`
+
+const ProductsHeading = styled.div`
+  font-size: clamp(1.2rem, 5vw, 3rem);
+  text-align: center;
+  margin-bottom: 5rem;
+  color: #000;
+`
+const ProductWrapper = styled.div``
